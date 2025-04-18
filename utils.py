cd53a92968b3f189539587fb2cb3e10e33263fa2
infoai1 <info@spiritualmessage.org>
2025-04-18 15:03:56 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,20 @@
+import logging
+import nltk
+import tiktoken
+import streamlit as st
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+)
+
+@st.cache_resource
+def ensure_punkt():
+    try:
+        nltk.data.find("tokenizers/punkt")
+    except LookupError:
+        nltk.download("punkt", quiet=True)
+
+@st.cache_resource
+def get_tokenizer(name: str = "cl100k_base"):
+    return tiktoken.get_encoding(name)
